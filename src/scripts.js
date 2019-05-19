57da50432784ef88b42df6631a831bb801ed8793
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-05-20 08:09:01 +1000

    included a icon automatic timer switch - shows the daily emotions of programming

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,10 @@
+
+
+
+// ======================================================================
+// Navigation 
+// ======================================================================
+
 function hide_and_show() {
   var x = document.getElementById("dropdown_content");
   if (x.style.display === "none") {
@@ -6,6 +13,10 @@ function hide_and_show() {
     x.style.display = "none";
   }
 }
+
+// ======================================================================
+// Aside Links
+// ======================================================================
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -26,3 +37,37 @@ window.onclick = function (event) {
     }
   }
 }
+
+
+
+
+
+// ======================================================================
+// Smile Function
+// ======================================================================
+
+function smile() {
+  var a;
+  a = document.getElementById("smile-sad");
+  a.innerHTML = "&#xf118;"; 
+  setTimeout(function () {
+      a.innerHTML = "&#xf4da;";
+    }, 1000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf119;";
+    }, 2000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf11a;";
+    }, 3000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf57f;";
+    }, 4000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf579;";
+    }, 5000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf583;";
+    }, 6000);
+}
+smile();
+setInterval(smile, 7000);
